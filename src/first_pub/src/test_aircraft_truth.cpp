e387b5ec4fa3ce63ac2f52116c04c0ff526f4a19
Carter Noh <carter.noh08@gmail.com>
2023-04-21 15:41:17 -0600

    trhuth should be working but not tested yet

--- src/first_pub/src/test_aircraft_truth.cpp
+++ src/first_pub/src/test_aircraft_truth.cpp
@@ -29,6 +29,8 @@ class AircraftTruth : public rclcpp::Node
             // get the info from the gazebo message
             auto gazeboPosition = msg.pose.pose.position;
             auto gazeboOrient = msg.pose.pose.orientation;
+            auto gazeboLinVelocity = msg.twist.twist.linear;
+            auto gazeboAngVelocity = msg.twist.twist.angular;
 
             // set up the state message object
             auto truthState = State();
@@ -41,8 +43,43 @@ class AircraftTruth : public rclcpp::Node
             truthState.position[1] = gazeboPosition.y;
             truthState.position[2] = gazeboPosition.z;
 
+            truthState.quat[0]= gazeboOrient.w;
+            truthState.quat[1]= gazeboOrient.x;
+            truthState.quat[2]= gazeboOrient.y;
+            truthState.quat[3]= gazeboOrient.z;
+            truthState.quat_valid = true
 
+            truthState.phi = atan2(2*(gazeboOrient.w * gazeboOrient.x + gazeboOrient.y * gazeboOrient.z), gazeboOrient.w^2 + gazeboOrient.z^2 - gazeboOrient.x^2 - gazeboOrient.y^2)
+            truthState.theta = asin(2*(gazeboOrient.w * gazeboOrient.y - gazeboOrient.x * gazeboOrient.z))
+            truthState.psi = atan2(2*(gazeboOrient.w * e3 + gazeboOrient.x * gazeboOrient.y), gazeboOrient.w^2 + gazeboOrient.x^2 - gazeboOrient.y^2 - gazeboOrient.z^2)
 
+            truthState.u = gazeboLinVelocity.x;
+            truthState.v = gazeboLinVelocity.y;
+            truthState.w = gazeboLinVelocity.z;
+
+            truthState.p = gazeboAngVelocity.x;
+            truthState.q = gazeboAngVelocity.y;
+            truthState.r = gazeboAngVelocity.z;
+
+            truthState.wn = 0; //not doing wind for now
+            truthState.we = 0; 
+            double wd = 0; 
+
+            double ur = u - truthState.wn;
+            double vr = v - truthState.we;
+            double wr = w - wd;
+
+            truthState.va = sqrt(pow(ur, 2.0) + pow(vr, 2.0) + pow(wr, 2.0));
+            truthState.chi = atan2(msg.Va*sin(msg.psi), msg.Va*cos(msg.psi));
+            truthState.alpha = atan2(wr, ur);
+            truthState.beta = asin(vr/msg.Va);
+
+            truthState.psi_deg = fmod(truthState.psi, 2.0*M_PI)*180.0 / M_PI; //-360 to 360
+            truthState.psi_deg += (truthState.psi_deg < -180.0 ? 360.0 : 0.0);
+            truthState.psi_deg -= (truthState.psi_deg > 180.0 ? 360.0 : 0.0);
+            truthState.chi_deg = fmod(truthState.chi, 2.0*M_PI)*180.0 / M_PI; //-360 to 360
+            truthState.chi_deg += (truthState.chi_deg < -180.0 ? 360.0 : 0.0);
+            truthState.chi_deg -= (truthState.chi_deg > 180.0 ? 360.0 : 0.0);
 
             RCLCPP_INFO(this->get_logger(), "Sub heard: '%s'", msg.child_frame_id.c_str());
             publisher_->publish(truthState);
