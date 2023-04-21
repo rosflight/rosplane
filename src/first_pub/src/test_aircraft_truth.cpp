68e94cdd480eb9ac4378e01607b0ce4e3a0f3cb0
Carter Noh <carter.noh08@gmail.com>
2023-04-21 16:03:22 -0600

    Truth is working!

--- src/first_pub/src/test_aircraft_truth.cpp
+++ src/first_pub/src/test_aircraft_truth.cpp
@@ -4,6 +4,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "rosplane2_msgs/msg/state.hpp"
+#include <cmath>
 
 using std::placeholders::_1;
 using nav_msgs::msg::Odometry;
@@ -15,8 +16,8 @@ class AircraftTruth : public rclcpp::Node
         AircraftTruth()
         : Node("test_aircraft_truth")
         {
-            subscription_ = this->create_subscription<Odometry>("gazebo_truth", 10, std::bind(&AircraftTruth::topic_callback, this, _1));
-            publisher_ = this->create_publisher<State>("rosplane2_truth", 10);
+            subscription_ = this->create_subscription<nav_msgs::msg::Odometry>("truth/odometry", 10, std::bind(&AircraftTruth::topic_callback, this, _1));
+            publisher_ = this->create_publisher<rosplane2_msgs::msg::State>("state", 10);
 
         }
 
@@ -47,11 +48,11 @@ class AircraftTruth : public rclcpp::Node
             truthState.quat[1]= gazeboOrient.x;
             truthState.quat[2]= gazeboOrient.y;
             truthState.quat[3]= gazeboOrient.z;
-            truthState.quat_valid = true
+            truthState.quat_valid = true;
 
-            truthState.phi = atan2(2*(gazeboOrient.w * gazeboOrient.x + gazeboOrient.y * gazeboOrient.z), gazeboOrient.w^2 + gazeboOrient.z^2 - gazeboOrient.x^2 - gazeboOrient.y^2)
-            truthState.theta = asin(2*(gazeboOrient.w * gazeboOrient.y - gazeboOrient.x * gazeboOrient.z))
-            truthState.psi = atan2(2*(gazeboOrient.w * e3 + gazeboOrient.x * gazeboOrient.y), gazeboOrient.w^2 + gazeboOrient.x^2 - gazeboOrient.y^2 - gazeboOrient.z^2)
+            truthState.phi = atan2(2*(gazeboOrient.w * gazeboOrient.x + gazeboOrient.y * gazeboOrient.z), pow(gazeboOrient.w,2.0) + pow(gazeboOrient.z,2.0) - pow(gazeboOrient.x,2.0) - pow(gazeboOrient.y,2.0));
+            truthState.theta = asin(2*(gazeboOrient.w * gazeboOrient.y - gazeboOrient.x * gazeboOrient.z));
+            truthState.psi = atan2(2*(gazeboOrient.w * gazeboOrient.z + gazeboOrient.x * gazeboOrient.y), pow(gazeboOrient.w,2.0) + pow(gazeboOrient.x,2.0) - pow(gazeboOrient.y,2.0) - pow(gazeboOrient.z,2.0));
 
             truthState.u = gazeboLinVelocity.x;
             truthState.v = gazeboLinVelocity.y;
@@ -65,14 +66,14 @@ class AircraftTruth : public rclcpp::Node
             truthState.we = 0; 
             double wd = 0; 
 
-            double ur = u - truthState.wn;
-            double vr = v - truthState.we;
-            double wr = w - wd;
+            double ur = truthState.u - truthState.wn;
+            double vr = truthState.v - truthState.we;
+            double wr = truthState.w - wd;
 
             truthState.va = sqrt(pow(ur, 2.0) + pow(vr, 2.0) + pow(wr, 2.0));
-            truthState.chi = atan2(msg.Va*sin(msg.psi), msg.Va*cos(msg.psi));
+            truthState.chi = atan2(truthState.va*sin(truthState.psi), truthState.va*cos(truthState.psi));
             truthState.alpha = atan2(wr, ur);
-            truthState.beta = asin(vr/msg.Va);
+            truthState.beta = asin(vr/truthState.va);
 
             truthState.psi_deg = fmod(truthState.psi, 2.0*M_PI)*180.0 / M_PI; //-360 to 360
             truthState.psi_deg += (truthState.psi_deg < -180.0 ? 360.0 : 0.0);
@@ -81,7 +82,7 @@ class AircraftTruth : public rclcpp::Node
             truthState.chi_deg += (truthState.chi_deg < -180.0 ? 360.0 : 0.0);
             truthState.chi_deg -= (truthState.chi_deg > 180.0 ? 360.0 : 0.0);
 
-            RCLCPP_INFO(this->get_logger(), "Sub heard: '%s'", msg.child_frame_id.c_str());
+            //RCLCPP_INFO(this->get_logger(), "Sub heard: '%s'", msg.child_frame_id.c_str());
             publisher_->publish(truthState);
         }
 
--- src/rosplane2/src/test_aircraft_truth.cpp
+++ src/rosplane2/src/test_aircraft_truth.cpp
@@ -15,8 +15,8 @@ class AircraftTruth : public rclcpp::Node
         AircraftTruth()
         : Node("test_aircraft_truth")
         {
-            subscription_ = this->create_subscription<Odometry>("gazebo_truth", 10, std::bind(&AircraftTruth::topic_callback, this, _1));
-            publisher_ = this->create_publisher<State>("rosplane2_truth", 10);
+            subscription_ = this->create_subscription<nav_msgs::msg::Odometry>("truth/odometry", 10, std::bind(&AircraftTruth::topic_callback, this, _1));
+            publisher_ = this->create_publisher<rosplane2_msgs::msg::State>("state", 10);
 
         }
 
@@ -29,6 +29,8 @@ class AircraftTruth : public rclcpp::Node
             // get the info from the gazebo message
             auto gazeboPosition = msg.pose.pose.position;
             auto gazeboOrient = msg.pose.pose.orientation;
+            auto gazeboLinVelocity = msg.twist.twist.linear;
+            auto gazeboAngVelocity = msg.twist.twist.angular;
 
             // set up the state message object
             auto truthState = State();
@@ -41,10 +43,45 @@ class AircraftTruth : public rclcpp::Node
             truthState.position[1] = gazeboPosition.y;
             truthState.position[2] = gazeboPosition.z;
 
+            truthState.quat[0]= gazeboOrient.w;
+            truthState.quat[1]= gazeboOrient.x;
+            truthState.quat[2]= gazeboOrient.y;
+            truthState.quat[3]= gazeboOrient.z;
+            truthState.quat_valid = true;
 
+            truthState.phi = atan2(2*(gazeboOrient.w * gazeboOrient.x + gazeboOrient.y * gazeboOrient.z), pow(gazeboOrient.w,2.0) + pow(gazeboOrient.z,2.0) - pow(gazeboOrient.x,2.0) - pow(gazeboOrient.y,2.0));
+            truthState.theta = asin(2*(gazeboOrient.w * gazeboOrient.y - gazeboOrient.x * gazeboOrient.z));
+            truthState.psi = atan2(2*(gazeboOrient.w * gazeboOrient.z + gazeboOrient.x * gazeboOrient.y), pow(gazeboOrient.w,2.0) + pow(gazeboOrient.x,2.0) - pow(gazeboOrient.y,2.0) - pow(gazeboOrient.z,2.0));
 
+            truthState.u = gazeboLinVelocity.x;
+            truthState.v = gazeboLinVelocity.y;
+            truthState.w = gazeboLinVelocity.z;
 
-            RCLCPP_INFO(this->get_logger(), "Sub heard: '%s'", msg.child_frame_id.c_str());
+            truthState.p = gazeboAngVelocity.x;
+            truthState.q = gazeboAngVelocity.y;
+            truthState.r = gazeboAngVelocity.z;
+
+            truthState.wn = 0; //not doing wind for now
+            truthState.we = 0; 
+            double wd = 0; 
+
+            double ur = truthState.u - truthState.wn;
+            double vr = truthState.v - truthState.we;
+            double wr = truthState.w - wd;
+
+            truthState.va = sqrt(pow(ur, 2.0) + pow(vr, 2.0) + pow(wr, 2.0));
+            truthState.chi = atan2(truthState.va*sin(truthState.psi), truthState.va*cos(truthState.psi));
+            truthState.alpha = atan2(wr, ur);
+            truthState.beta = asin(vr/truthState.va);
+
+            truthState.psi_deg = fmod(truthState.psi, 2.0*M_PI)*180.0 / M_PI; //-360 to 360
+            truthState.psi_deg += (truthState.psi_deg < -180.0 ? 360.0 : 0.0);
+            truthState.psi_deg -= (truthState.psi_deg > 180.0 ? 360.0 : 0.0);
+            truthState.chi_deg = fmod(truthState.chi, 2.0*M_PI)*180.0 / M_PI; //-360 to 360
+            truthState.chi_deg += (truthState.chi_deg < -180.0 ? 360.0 : 0.0);
+            truthState.chi_deg -= (truthState.chi_deg > 180.0 ? 360.0 : 0.0);
+
+            //RCLCPP_INFO(this->get_logger(), "Sub heard: '%s'", msg.child_frame_id.c_str());
             publisher_->publish(truthState);
         }
 
