a74c0c750b4b2db33be175c84132d08ad9ddef4b
ian <ian.young.reid@gmail.com>
2023-08-10 10:44:04 -0600

    Updated sim launch file to use standard launch and then add visualization.

--- src/rosplane2_sim/launch/sim.launch.py
+++ src/rosplane2_sim/launch/sim.launch.py
@@ -1,6 +1,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
 import os
 
 def generate_launch_description():
@@ -17,37 +19,17 @@ def generate_launch_description():
     # Flag for enabling/disabling use of simulation time instead of wall clock
     use_sim_time = True
 
+    base_launch_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('rosplane2'),
+                'launch/rosplane2.launch.py'
+            )
+        )
+    )
+
     return LaunchDescription([
-        Node(
-            package='rosplane2',
-            executable='rosplane2_controller',
-            name='autopilot',
-        ),
-        Node(
-            package='rosplane2',
-            executable='rosplane2_path_follower',
-            name='path_follower',
-        ),
-        Node(
-            package='rosplane2',
-            executable='rosplane2_path_manager',
-            name='path_manager',
-        ),
-        Node(
-            package='rosplane2',
-            executable='rosplane2_path_planner',
-            name='path_planner',
-        ),
-        Node(
-            package='rosplane2',
-            executable='rosplane2_estimator_node',
-            name='estimator'
-        ),
-        Node (
-            package = 'rosplane2_sim',
-            executable='rosplane2_gazebo_truth_publisher',
-            name='gazebo_truth'
-        ),
+        base_launch_include,
         Node(
             package='data_viz',
             executable='viz_data',
