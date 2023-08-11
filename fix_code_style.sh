23a44c2c4e2ac463257d0ad048a61894e24fca15
Brandon Sutherland <brandonsutherland2@gmail.com>
2023-08-11 08:45:54 -0600

    Copied files for license, github CI, clang-format, and github templates from rosflight2

---
+++ fix_code_style.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Change the working directory to the script's directory
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+cd $SCRIPTPATH
+
+# Find all files with ".hpp" or ".cpp" extensions in the current directory and subdirectories,
+# excluding certain paths (.rosflight/include/mavlink/v1.0/, ./rosflight_firmware/firmware/, and ./.git)
+find . -iname "*.hpp" -o -iname "*.cpp" | \
+egrep -v "^(.rosflight/include/mavlink/v1.0/|./rosflight_firmware/firmware/|./.git)" | \
+
+# Format the files according to the rules specified in .clang-format
+xargs clang-format -i
