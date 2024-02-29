aca1e1b5f9fa767645524d738b33aaab43597359
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-02-29 13:54:37 -0700

    created rosplane_tuning package, moved signal_generator

--- rosplane_tuning/launch/rosplane_tuning.launch.py
+++ rosplane_tuning/launch/rosplane_tuning.launch.py
@@ -16,10 +16,10 @@ def generate_launch_description():
     for arg in sys.argv:
         if arg.startswith("control_type:="):
             control_type = arg.split(":=")[1]
-        
+
         if arg.startswith("aircraft:="):
             aircraft = arg.split(":=")[1]
-    
+
     autopilot_params = os.path.join(
         rosplane_dir,
         'params',
