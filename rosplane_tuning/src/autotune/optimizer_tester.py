0775822a33fc569d3c9e9ddd6e582fd3f5791849
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-04-11 15:02:08 -0600

    fixed error in FakeRollController

--- rosplane_tuning/src/autotune/optimizer_tester.py
+++ rosplane_tuning/src/autotune/optimizer_tester.py
@@ -4,37 +4,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def Matyas(x):
-    # Matyas function
-    return 0.26 * (x[0] ** 2 + x[1] ** 2) - 0.48 * x[0] * x[1]
-
-def Circle(x):
-    # Circle function
-    return x[0] ** 2 + x[1] ** 2
-
-def FakeRollController(x):
+def FakeRollController(x, include_noise):
     # Function that roughly mimics the roll controller landscape
-    std = 0.1
-    rotation = np.deg2rad(45)
-    x_rot = x[0] * np.cos(rotation) - x[1] * np.sin(rotation)
-    y_rot = x[0] * np.sin(rotation) + x[1] * np.cos(rotation)
-    return 15*(x_rot - 0.2)**2 + 3*(y_rot - 0.03)**2 + np.random.normal(0, std)
-
-function = FakeRollController
+    std = 0.1 if include_noise else 0.0
+    rotation = np.deg2rad(-40)
+    x_trans = x[0] - 0.2
+    y_trans = x[1] - 0.03
+    x_rot = x_trans * np.cos(rotation) + y_trans * np.sin(rotation)
+    y_rot = x_trans * -np.sin(rotation) + y_trans * np.cos(rotation)
+    return 15*x_rot**2 + 3*y_rot**2 + np.random.normal(0, std)
 
 # Initialize optimizer
 curr_points = np.array([[0.06, 0.04]])  # Initial point
 optimization_params = {'mu_1': 1e-4,
                        'mu_2': 0.5,
                        'sigma': 1.5,
-                       'alpha': 1.0,
-                       'tau': 1e-2,
-                       'h': 1e-2}
+                       'alpha': 0.05,
+                       'tau': 0.1,
+                       'h': 0.05}
 optimizer = Optimizer(curr_points[0], optimization_params)
+num_repeats = 5
 
 # Print initial point and value
 print('Initial point: {}'.format(curr_points[0]))
-print('Initial value: {}'.format(function(curr_points[0])))
+print('Initial value: {}'.format(FakeRollController(curr_points[0], False)))
 
 # Run optimization
 all_points = []
@@ -46,7 +39,7 @@ while not optimizer.optimization_terminated():
     # Calculate error for current points
     error = []
     for point in curr_points:
-        error.append(function(point))
+        error.append(np.average([FakeRollController(point, True) for _ in range(num_repeats)]))
     error = np.array(error)
     # Pass points to optimizer
     curr_points = optimizer.get_next_parameter_set(error)
@@ -61,24 +54,19 @@ all_points = np.array(all_points)
 x_0_points = np.array(x_0_points)
 
 print('Optimization terminated with status: {}'.format(optimizer.get_optimization_status()))
-print('Total number of points tested: {}'.format(len(all_points)))
+print('Total number of points tested: {}'.format(len(all_points) * num_repeats))
 print('Final point: {}'.format(curr_points[0]))
-print('Final value: {}'.format(function(curr_points[0])))
+print('Final value: {}'.format(FakeRollController(curr_points[0], False)))
 
 
 # Plot the function with the optimization path
 range = 0.5
-x_min = np.min(all_points[:, 0]) - range/2
-x_max = np.max(all_points[:, 0]) + range/2
-y_min = np.min(all_points[:, 1]) - range/2
-y_max = np.max(all_points[:, 1]) + range/2
-x = np.linspace(x_min, x_max, 100)
-y = np.linspace(y_min, y_max, 100)
+x = np.linspace(-0.1, 0.5, 100)
+y = np.linspace(-0.3, 0.3, 100)
 X, Y = np.meshgrid(x, y)
-Z = function([X, Y])
+Z = FakeRollController([X, Y], False)
 plt.contour(X, Y, Z, 50)
-plt.scatter(x_0_points[:, 0], x_0_points[:, 1], color='g', marker='x', s=200)
-plt.plot(all_points[:, 0], all_points[:, 1], marker='o', color='r')
+plt.plot(x_0_points[:, 0], x_0_points[:, 1], color='b', marker='o')
 # Lock the x and y axis to be equal
 plt.axis('equal')
 plt.show()
