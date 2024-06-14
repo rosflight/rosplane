62c35d11de473f08a11a4320a96bb4278cc18bd1
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-06-14 17:12:28 -0600

    Set up framework mixing command input
    (cherry picked from commit 555b65fe3253e36cee1f8d9139698d6614334204)

--- rosplane/launch/rosplane.launch.py
+++ rosplane/launch/rosplane.launch.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import launch.actions
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
     # Create the package directory
@@ -10,7 +12,7 @@ def generate_launch_description():
 
     # Determine the appropriate control scheme.
     control_type = "default"
-    aircraft = "skyhunter" # Default aircraft 
+    aircraft = "skyhunter"  # Default aircraft
     init_lat = "0.0" # init lat if seeding the estimator (typically not done)
     init_long = "0.0"
     init_alt = "0.0"
@@ -19,10 +21,10 @@ def generate_launch_description():
     for arg in sys.argv:
         if arg.startswith("control_type:="):
             control_type = arg.split(":=")[1]
-        
+
         if arg.startswith("aircraft:="):
             aircraft = arg.split(":=")[1]
-        
+
         if arg.startswith("init_lat:="):
             init_lat = float(arg.split(":=")[1])
             assert isinstance(init_lat, float)
@@ -37,7 +39,7 @@ def generate_launch_description():
             init_alt = float(arg.split(":=")[1])
             assert isinstance(init_alt, float)
             init_alt = str(init_alt)
-    
+
     autopilot_params = os.path.join(
         rosplane_dir,
         'params',
@@ -45,25 +47,39 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            'controller_commands_subscriber_remap',
+            default_value='/controller_commands',
+        ),
         Node(
             package='rosplane',
             executable='rosplane_controller',
             name='autopilot',
-            parameters = [autopilot_params],
-            output = 'screen',
-            arguments = [control_type]
+            parameters=[autopilot_params],
+            output='screen',
+            arguments=[control_type],
+            remappings=[
+                ('/controller_commands', launch.substitutions.LaunchConfiguration('controller_commands_subscriber_remap')),
+            ]
+        ),
+        launch.actions.DeclareLaunchArgument(
+            'controller_commands_publisher_remap',
+            default_value='/controller_commands',
         ),
         Node(
             package='rosplane',
             executable='rosplane_path_follower',
             name='path_follower',
-            parameters = [autopilot_params],
+            parameters=[autopilot_params],
+            remappings=[
+                ('/controller_commands', launch.substitutions.LaunchConfiguration('controller_commands_publisher_remap'))
+            ]
         ),
         Node(
             package='rosplane',
             executable='rosplane_path_manager',
             name='path_manager',
-            parameters = [autopilot_params],
+            parameters=[autopilot_params],
         ),
         Node(
             package='rosplane',
