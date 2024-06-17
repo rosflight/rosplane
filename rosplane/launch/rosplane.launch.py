a18d6c336cfdf879df740a7232baeb962d789e9a
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-06-17 12:52:24 -0600

    Revert "Set up framework mixing command input"
    This reverts commit 555b65fe3253e36cee1f8d9139698d6614334204.

--- rosplane/launch/rosplane.launch.py
+++ rosplane/launch/rosplane.launch.py
@@ -1,10 +1,8 @@
 import os
 import sys
-import launch.actions
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-
 
 def generate_launch_description():
     # Create the package directory
@@ -12,15 +10,15 @@ def generate_launch_description():
 
     # Determine the appropriate control scheme.
     control_type = "default"
-    aircraft = "skyhunter"  # Default aircraft
+    aircraft = "skyhunter" # Default aircraft 
 
     for arg in sys.argv:
         if arg.startswith("control_type:="):
             control_type = arg.split(":=")[1]
-
+        
         if arg.startswith("aircraft:="):
             aircraft = arg.split(":=")[1]
-
+    
     autopilot_params = os.path.join(
         rosplane_dir,
         'params',
@@ -28,39 +26,25 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        launch.actions.DeclareLaunchArgument(
-            'controller_commands_subscriber_remap',
-            default_value='/controller_commands',
-        ),
         Node(
             package='rosplane',
             executable='rosplane_controller',
             name='autopilot',
-            parameters=[autopilot_params],
-            output='screen',
-            arguments=[control_type],
-            remappings=[
-                ('/controller_commands', launch.substitutions.LaunchConfiguration('controller_commands_subscriber_remap')),
-            ]
-        ),
-        launch.actions.DeclareLaunchArgument(
-            'controller_commands_publisher_remap',
-            default_value='/controller_commands',
+            parameters = [autopilot_params],
+            output = 'screen',
+            arguments = [control_type]
         ),
         Node(
             package='rosplane',
             executable='rosplane_path_follower',
             name='path_follower',
-            parameters=[autopilot_params],
-            remappings=[
-                ('/controller_commands', launch.substitutions.LaunchConfiguration('controller_commands_publisher_remap'))
-            ]
+            parameters = [autopilot_params],
         ),
         Node(
             package='rosplane',
             executable='rosplane_path_manager',
             name='path_manager',
-            parameters=[autopilot_params],
+            parameters = [autopilot_params],
         ),
         Node(
             package='rosplane',
