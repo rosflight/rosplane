e382a743c8b2d25110277b506eea8e343be12ea4
Ian Reid <ian.young.reid@gmail.com>
2025-01-07 15:00:48 -0700

    Fixed uniitialtized input problem with the barometer.
    The barometer measurement would sometimes not be fully initialized
    before the main loop of the estimate. This means that the baro
    measurement was random.

--- rosplane/src/estimator_ros.cpp
+++ rosplane/src/estimator_ros.cpp
@@ -54,6 +54,7 @@ EstimatorROS::EstimatorROS()
   param_filepath_ = full_path.string();
 
   input_.diff_pres = 0.0; // Initalize the differential_pressure measurement to zero.
+  input_.static_pres = 0.0; // Initalize the differential_pressure measurement to zero.
 
   set_timer();
 }
