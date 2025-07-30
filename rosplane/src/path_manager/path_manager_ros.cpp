7efcd6dd01d40a44b4654fff6e44bd5ab75623fd
Ian Reid <ian.young.reid@gmail.com>
2025-07-30 09:09:22 -0600

    Updated the license and author headers.

--- rosplane/src/path_manager/path_manager_ros.cpp
+++ rosplane/src/path_manager/path_manager_ros.cpp
@@ -1,8 +1,46 @@
-#include <iostream>
-#include <limits>
+/*
+ * Software License Agreement (BSD-3 License)
+ *
+ * Copyright (c) 2025 Ian Reid, BYU MAGICC Lab.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#include <rclcpp/logging.hpp>
-#include <rclcpp/rclcpp.hpp>
+/**
+ * @file path_manager_ros.cpp
+ *
+ * Base class definition for autopilot path follower in chapter 10 of UAVbook, see http://uavbook.byu.edu/doku.php
+ * implements the ROS interfaces for path management
+ *
+ * @author Gary Ellingson <gary.ellingson@byu.edu>
+ * adapted by Judd Mehr and Brian Russel for ROSplane software
+ * @author Ian Reid <ian.young.reid@gmail.com>
+ */
 
 #include "path_manager/path_manager_dubins_fillets.hpp"
 
--- rosplane/src/path_planner/path_planner_waypoints.cpp
+++ rosplane/src/path_planner/path_planner_waypoints.cpp
@@ -1,18 +1,44 @@
-#include <cmath>
+/*
+ * Software License Agreement (BSD-3 License)
+ *
+ * Copyright (c) 2025 Ian Reid and Jacob Moore, BYU MAGICC Lab.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#include <rclcpp/executors.hpp>
-#include <rclcpp/logging.hpp>
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp/service.hpp>
-#include <rclcpp/utilities.hpp>
-#include <rosflight_msgs/srv/param_file.hpp>
-#include <std_srvs/srv/trigger.hpp>
-#include <yaml-cpp/yaml.h>
-
-#include "param_manager.hpp"
-#include "rosplane_msgs/msg/state.hpp"
-#include "rosplane_msgs/msg/waypoint.hpp"
-#include "rosplane_msgs/srv/add_waypoint.hpp"
+/**
+ * @file path_planner_waypoints.cpp
+ *
+ * Implements the ROS interfaces and functionality for sending and receiving waypoints.
+ *
+ * @author Ian Reid <ian.young.reid@gmail.com>
+ * @author Ian Reid <jacobmoor2@gmail.com>
+ */
 
 #include "path_planner/path_planner_waypoints.hpp"
 
